148194d189dfb1cda815f7f3e8d73d6003e9abd1
白月秋见心 <ourweijiang@gmail.com>
2016-03-27 14:07:58 +0800

    征文链接更改

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ ENTER_CHAT_STATE_TEXT = u"已进入自动陪聊模式\n回复你想要说的话�
 
 CHAT_TIMEOUT_TEXT = u"那么久不理小喵[可怜]\n自动退出陪聊模式啦~\n\n回复“陪聊”可以重新进入"
 
-CONTRIBUTION_TEXT = u'欢迎到小喵投稿~\n\n规则说明：<a href="http://mp.weixin.qq.com/s?__biz=MzI1MzA1MzQ0MA==&mid=405371033&idx=2&sn=91036e4e1020bf0d2a8fb03c9f5c65ca">点击这里</a>\n\n投稿邮箱：xiaomiao@gxgk.cc\n\n灵感突现：<a href="http://zw.gxgk.cc/Home/Contribute/article.html">点击这里</a>'
+CONTRIBUTION_TEXT = u'欢迎到小喵投稿~\n\n规则说明：<a href="http://mp.weixin.qq.com/s?__biz=MzI1MzA1MzQ0MA==&mid=405373826&idx=1&sn=577a09fc79cab3bc68939d895abc4807">点击这里</a>\n\n投稿邮箱：xiaomiao@gxgk.cc\n\n灵感突现：<a href="http://zw.gxgk.cc/Home/Contribute/article.html">点击这里</a>'
 
 TEASING_TIMEOUT_TEXT = u'超时啦\n自动退出吐槽模式啦~'
 
